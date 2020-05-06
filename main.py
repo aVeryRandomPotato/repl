d3494376b0a0c8687aca83732e68786355ab0d73
aVeryRandomPotato <chase.stancik@gmail.com>
2020-05-06 00:58:19 +0000

    added random age

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import random
+
 my_name = "Chase"
 
 print("Hello " + my_name)
 
 age = 10
 
-if age > 12:
-  print("You're a teenager")
-else:
-  print("You're a pre-teen")
+def ageChecker(x):
+  if age > 12:
+    print("You're a teenager - " + x)
+  else:
+    print("You're a pre-teen - " + x)
+
+ageChecker("var")
+
+age = random.randint(10,20)
+
+ageChecker("random")
